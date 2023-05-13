f785f51887c043fbd10413eb0897c16381885972
KacperW <kwoznicki10@gmail.com>
2023-05-13 16:53:26 +0200

    SliderFix

--- components/article-list/Article.js
+++ components/article-list/Article.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import styles from "./Article.module.scss";
-import { useEffect,useState } from 'react';
-export const Article = ({news}) => {
+export const Article = ({news,currentVisibleArticle}) => {
   return (
-    <div className={styles.listBoxes}>
+    <div className={styles.listBoxes} style={{
+      
+      '@media screen (min-width: 940px)': {
+        transform: `translateX(calc(${currentVisibleArticle} / 3 * -100%))`
+      },
+      '@media screen (min-width: 680px)': {
+        transform: `translateX(calc(${currentVisibleArticle} / 2 * -100%))`
+      },
+      '@media screen (min-width: 1320px)': {
+        transform: `translateX(calc(${currentVisibleArticle} / 4 * -100%))`
+      },
+      transform: `translateX(calc(${currentVisibleArticle} / 1 * -100%))`
+    }}>
 
           {news.map(({title,body,topic}) =>{
             return <div key={title} className={styles.box} >
--- components/article-list/ArticleList.js
+++ components/article-list/ArticleList.js
@@ -2,8 +2,8 @@ import styles from "./Article.module.scss";
 import LeftArrow from "../../images/leftarrow.svg";
 import RightArrow from "../../images/rightarrow.svg";
 import Image from "next/image";
-import useWindowDimensions from "../../hooks/useWindowDimensions.js";
-import { useState, useEffect } from "react";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+import { useEffect, useState } from "react";
 import { Article } from "./Article";
 
 const ArticleList = () => {
@@ -58,21 +58,36 @@ const ArticleList = () => {
       body: "Blah Blah Blah",
       topic: "Flower",
     },
+    {
+      title: "Akt10",
+      body: "Blah Blah Blah",
+      topic: "Flower",
+    },
   ];
-
-  // const { width } = useWindowDimensions();
+  const { width } = useWindowDimensions();
   const [currentVisibleArticle, setCurrentVisibleArticle] = useState(0);
-
-  const [direction, setDirection] = useState("");
+  useEffect(() => {
+    setCurrentVisibleArticle(0);
+  }, [width]);
   const nextNews = (e) => {
     e.preventDefault();
-    setCurrentVisibleArticle((prev) => prev + 1);
+    if(currentVisibleArticle === news.length - (
+      (width > 1320) ? 9 :
+      (width <= 1320 && width > 940) ? 8 :
+      (width <= 940 && width > 680) ? 6 :
+      (width <= 680) ? 1 :
+      0
+    )){
+      return null;
+    }
+
+    setCurrentVisibleArticle(prev => prev + 1);
     console.log("next");
   };
 
   const prevNews = (e) => {
     e.preventDefault();
-    setCurrentVisibleArticle((prev) => prev - 1);
+    setCurrentVisibleArticle(prev => prev - 1);
     console.log("back");
   };
 
@@ -81,7 +96,7 @@ const ArticleList = () => {
       className={styles.listContainer}
       currentVisibleArticle={currentVisibleArticle}
     >
-      <Article news={news} />
+      <Article news={news} currentVisibleArticle={currentVisibleArticle}/>
       <Image
         src={LeftArrow}
         className={styles.leftArrow}
