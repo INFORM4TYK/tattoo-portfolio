8bd5f32dd5f444efe8723ff83ad8549c62f05b78
KacperW <kwoznicki10@gmail.com>
2023-05-14 22:52:30 +0200

    AddReactHelmet

--- pages/galeria.js
+++ pages/galeria.js
@@ -1,8 +1,10 @@
 import styles from "../styles/Galeria.module.scss";
 import { news } from "../components/article-list/ArticleList";
+import CustomHelmet from "../components/CustomHelmet";
 export default function Galeria() {
   return (
     <div className={styles.container}>
+      <CustomHelmet Customtitle='Galeria' />
       {news.map(({ title, body, topic }) => {
         return (
           <div key={title} className={styles.boxes}>
